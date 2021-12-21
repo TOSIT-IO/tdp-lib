1eef62ad632dc4d397fd62b467ee2550a2d88ca1
Romain Pignolet <romain.pignolet@dgfip.finances.gouv.fr>
2021-12-21 18:42:27 +0100

    core: refactor "dag" with a class

--- tdp/core/dag.py
+++ tdp/core/dag.py
@@ -12,45 +12,56 @@ from pathlib import Path
 import networkx as nx
 
 
-def load_components():
-    # TODO load from multiple files
-    component_file_path = Path(__file__).with_name("components.yml")
-    with component_file_path.open("r") as component_file:
-        content = yaml.load(component_file, Loader=Loader) or {}
-         
-        # TODO sort content
-        return content
+class Dag:
+    """Generate DAG with components dependencies"""
 
+    def __init__(self):
+        self._components = None
+        self._graph = None
 
-def load_dag(content):
-    DG = nx.DiGraph()
-    for component in content:
-        DG.add_node(component['name'])
+    @property
+    def components(self):
+        if self._components is not None:
+            return self._components
 
-    for component in content:
-        for dependency in component['depends_on']:
-            for checkcomp in content:
-                if dependency == checkcomp['name']:
-                    DG.add_edge(dependency, component['name'])
-                    break
-            else:
-                raise ValueError('Depedency ' + dependency + ' does not exist')
+        # TODO load from multiple files
+        component_file_path = Path(__file__).with_name("components.yml")
+        with component_file_path.open("r") as component_file:
+            self._components = yaml.load(component_file, Loader=Loader) or {}
 
-    if nx.is_directed_acyclic_graph(DG):
-        return DG
-    else:
-        raise ValueError('Not a DAG')
+            # TODO sort content
+            return self._components
 
+    @property
+    def graph(self):
+        if self._graph is not None:
+            return self._graph
 
-def get_actions_to_node(dag, node):
-    actions = list(nx.lexicographical_topological_sort(dag.subgraph(nx.ancestors(dag, node))))
-    actions.append(node)
-    return actions
+        DG = nx.DiGraph()
+        for component in self.components:
+            DG.add_node(component['name'])
 
+        for component in self.components:
+            for dependency in component['depends_on']:
+                for checkcomp in self.components:
+                    if dependency == checkcomp['name']:
+                        DG.add_edge(dependency, component['name'])
+                        break
+                else:
+                    raise ValueError('Depedency ' + dependency + ' does not exist')
+
+        if nx.is_directed_acyclic_graph(DG):
+            self._graph = DG
+            return self._graph
+        else:
+            raise ValueError('Not a DAG')
+
+    def get_action_to_node(self, node):
+        actions = list(nx.lexicographical_topological_sort(self.graph.subgraph(nx.ancestors(self.graph, node))))
+        actions.append(node)
+        return actions
 
 
 if __name__ == "__main__":
-    components = load_components()
-    dag = load_dag(components)
-
-    print(get_actions_to_node(dag, 'hdfs_init'))
+    dag = Dag()
+    print(dag.get_action_to_node('hdfs_init'))
