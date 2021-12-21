c0a1bbc7ed02b9b3c23ca66ac97ba65e1dffa12e
Romain Pignolet <romain.pignolet@dgfip.finances.gouv.fr>
2021-12-21 18:24:15 +0100

    setup: freeze dependencies and add "networkx" and "PyYAML"

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name='TDP Lib',
     version='0.0.1',
     install_requires=[
-        'pytest'
+        'pytest==6.2.5',
+        'ansible==2.9.27',
+        'networkx==2.6.3',
+        'PyYAML==6.0',
     ],
     packages=[
         'tdp.core',
