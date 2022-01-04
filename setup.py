75e02b0fd75bcafa9e60f1031f657717e0dbdad9
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-01-04 09:58:23 +0100

    setup: downgrade dependencies versions for python 3.6 compatibility

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     install_requires=[
         'pytest==6.2.5',
         'ansible==2.9.27',
-        'networkx==2.6.3',
+        'networkx==2.5.1',
         'PyYAML==6.0',
     ],
     packages=[
