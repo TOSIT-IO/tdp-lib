5e62f84ce05734352d30eea5e55dcef6b7a9bdfc
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-01-21 09:42:31 +0100

    devtools: add usage, improve argument handling, document usage

--- tdp/devtools/dag_debug.py
+++ tdp/devtools/dag_debug.py
@@ -1,5 +1,5 @@
+import argparse
 import networkx as nx
-import sys
 
 from tdp.core.dag import Dag
 from tdp.core.dag_dot import show
@@ -13,17 +13,40 @@ except ImportError as e:
     ) from e
 
 
-def debug_dag(node=None):
-    """Compute and display a graph. Add a node name to get a subgraph to the node.
+DAG_SUMMARY = (
+    "Compute and display a graph. Add a node name to get a subgraph to the node."
+)
 
-    This method will lookup in the program arguments to check wether there's a node if no node is specified.
+
+def debug_dag_args():
+    parser = argparse.ArgumentParser(description=DAG_SUMMARY)
+    parser.description
+    parser.add_argument(
+        "node",
+        type=str,
+        nargs="?",
+        default=None,
+        help="Node on which to produce ancestors graph",
+    )
+    return parser
+
+
+def debug_dag(node=None):
+    f"""{DAG_SUMMARY}
+
+    This function will lookup in the program arguments to check wether there's a node if no node is specified.
     Args:
-        node (str, optional): Node to which compute a graph. Defaults to None.
+        node (str, optional): Node on which to compute a graph. Defaults to None.
     """
+    if not node:
+        node = debug_dag_args().parse_args().node
+
     dag = Dag()
-    if not node and len(sys.argv) == 2:
-        node = sys.argv[1]
     graph = dag.graph
     if node:
-        graph = graph.subgraph(nx.ancestors(graph, node))
+        ancestors = nx.ancestors(graph, node)
+        # Nx ancestors only returns the ancestor, and node the selected node
+        # Add it to simplify visualization
+        ancestors.add(node)
+        graph = graph.subgraph(ancestors)
     show(graph)
