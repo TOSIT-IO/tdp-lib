28e00629667bd868a9406d12066be335ea7c0a9e
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-01-31 09:29:24 +0100

    core: simplify repository usage in repository manager

--- tdp/core/repository_manager.py
+++ tdp/core/repository_manager.py
@@ -11,14 +11,15 @@ class RepositoryManager:
     def __init__(self, path, repository_class=GitRepository):
         self.path = Path(path)
         self._repository_class = repository_class
+        self._repository = self._repository_class(self.path)
         self._lock = Lock()
 
     def init(self):
         with self._lock:
-            self._repository_class(self.path).init()
+            self._repository.init()
 
     @contextmanager
     def validate(self, message):
-        with self._lock, self._repository_class(self.path) as repository:
+        with self._lock, self._repository as repository:
             yield repository
             repository.validate(message)
