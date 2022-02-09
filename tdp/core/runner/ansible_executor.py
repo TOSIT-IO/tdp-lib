7750d7a3bac2d969c1332377fdc6e967a245858d
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-02-09 11:48:15 +0100

    core: enable keyboard interrupt handling

--- tdp/core/runner/ansible_executor.py
+++ tdp/core/runner/ansible_executor.py
@@ -15,24 +15,30 @@ class AnsibleExecutor(Executor):
         self._rundir = run_directory
         self._dry = dry
 
+    def _execute_ansible_command(self, command):
+        with io.BytesIO() as byte_stream:
+            try:
+                res = subprocess.Popen(
+                    command,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                    cwd=self._rundir,
+                    universal_newlines=True,
+                )
+                for stdout_line in iter(res.stdout.readline, ""):
+                    print(stdout_line)
+                    byte_stream.write(bytes(stdout_line, "utf-8"))
+                state = StateEnum.SUCCESS if res.poll() == 0 else StateEnum.FAILURE
+            except KeyboardInterrupt:
+                logger.debug("KeyboardInterrupt caught")
+                byte_stream.write(b"\nKeyboardInterrupt")
+                return StateEnum.FAILURE, byte_stream.getvalue()
+            return state, byte_stream.getvalue()
+
     def execute(self, action):
         playbook_action = os.path.join(self._playdir, action + ".yml")
         command = ["ansible-playbook", playbook_action]
         if self._dry:
             logger.info("[DRY MODE] Ansible command: " + " ".join(command))
             return StateEnum.SUCCESS, b""
-
-        res = subprocess.Popen(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            cwd=self._rundir,
-            universal_newlines=True,
-        )
-        with io.BytesIO() as byte_stream:
-            for stdout_line in iter(res.stdout.readline, ""):
-                print(stdout_line)
-                byte_stream.write(bytes(stdout_line, "utf-8"))
-            logs = byte_stream.getvalue()
-        state = StateEnum.SUCCESS if res.poll() == 0 else StateEnum.FAILURE
-        return state, logs
+        return self._execute_ansible_command(command)
