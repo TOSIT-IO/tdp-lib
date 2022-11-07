15346eb15306e52c0f7d348a7d7bcbf0ceb1a2bd
gboutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-11-07 18:06:24 +0100

    fix: fix operation don't stop when failed

--- tdp/core/runner/test_deployment_runner.py
+++ tdp/core/runner/test_deployment_runner.py
@@ -19,9 +19,25 @@ class MockExecutor(Executor):
         return StateEnum.SUCCESS, f"{operation} LOG SUCCESS".encode("utf-8")
 
 
+class FailingExecutor(MockExecutor):
+    def __init__(self):
+        self.count = 0
+
+    def execute(self, operation):
+        if self.count > 0:
+            return StateEnum.FAILURE, f"{operation} LOG FAILURE".encode("utf-8")
+        self.count += 1
+        return super().execute(operation)
+
+
 @pytest.fixture
 def deployment_runner(minimal_collections, cluster_variables):
     return DeploymentRunner(minimal_collections, MockExecutor(), cluster_variables)
+
+
+@pytest.fixture
+def failing_deployment_runner(minimal_collections, cluster_variables):
+    return DeploymentRunner(minimal_collections, FailingExecutor(), cluster_variables)
 
 
 def test_deployment_plan_is_success(dag, deployment_runner):
@@ -49,4 +65,16 @@ def test_noop_deployment_plan_is_success(minimal_collections, deployment_runner)
 
     assert deployment_iterator.log.state == StateEnum.SUCCESS
     assert len(deployment_iterator.log.operations) == 0
+    assert len(deployment_iterator.log.services) > 0
+
+
+def test_failed_operation_stops(dag, failing_deployment_runner):
+    deployment_plan = DeploymentPlan.from_dag(dag, targets=["mock_init"])
+    deployment_iterator = failing_deployment_runner.run(deployment_plan)
+
+    for _ in deployment_iterator:
+        pass
+
+    assert deployment_iterator.log.state == StateEnum.FAILURE
+    assert len(deployment_iterator.log.operations) == 2
     assert len(deployment_iterator.log.services) > 0
