3eae9a905538c849612da5a0b6a42ee74b2f9afa
Romain Pignolet <romain.pignolet@dgfip.finances.gouv.fr>
2023-03-26 19:44:57 +0200

    test(core): refactor deployment conftest with generate_collection

--- tdp/core/deployment/conftest.py
+++ tdp/core/deployment/conftest.py
@@ -4,62 +4,50 @@
 import pytest
 import yaml
 
+from tdp.conftest import generate_collection
 from tdp.core.collection import Collection
 from tdp.core.collections import Collections
 from tdp.core.dag import Dag
 from tdp.core.variables import ClusterVariables
 
-MOCK_SERVICE_DAG = [
-    {"name": "mock_node_install", "depends_on": []},
-    {"name": "mock_node_config", "depends_on": ["mock_node_install"]},
-    {"name": "mock_node_start", "depends_on": ["mock_node_config"]},
-    {"name": "mock_node_init", "depends_on": ["mock_node_start"]},
-    {"name": "mock_install", "noop": True, "depends_on": ["mock_node_install"]},
-    {
-        "name": "mock_config",
-        "noop": True,
-        "depends_on": ["mock_install", "mock_node_config"],
-    },
-    {
-        "name": "mock_start",
-        "noop": True,
-        "depends_on": ["mock_config", "mock_node_start"],
-    },
-    {"name": "mock_init", "noop": True, "depends_on": ["mock_start", "mock_node_init"]},
-]
-
 
 @pytest.fixture(scope="session")
 def minimal_collections(tmp_path_factory):
-    collection = tmp_path_factory.mktemp("minimal_collection")
-
-    tdp_lib_dag = collection / "tdp_lib_dag"
-    playbooks = collection / "playbooks"
-    tdp_vars_defaults = collection / "tdp_vars_defaults"
-    mock_defaults = tdp_vars_defaults / "mock"
-
-    tdp_lib_dag.mkdir()
-    playbooks.mkdir()
-    tdp_vars_defaults.mkdir()
-    mock_defaults.mkdir()
-
-    with (tdp_lib_dag / "mock.yml").open("w") as fd:
-        fd.write(yaml.dump(MOCK_SERVICE_DAG))
-
-    # create empty playbooks
-    for operation in MOCK_SERVICE_DAG:
-        if operation["name"].endswith("_start"):
-            with (
-                playbooks / (operation["name"].rstrip("_start") + "_restart.yml")
-            ).open("w") as fd:
-                pass
-        with (playbooks / (operation["name"] + ".yml")).open("w") as fd:
-            pass
-
-    with (mock_defaults / "mock.yml").open("w") as fd:
-        fd.write(yaml.dump({"key": "value", "another_key": "another_value"}))
-
-    return Collections.from_collection_list([Collection.from_path(collection)])
+    collection_path = tmp_path_factory.mktemp("minimal_collection")
+    dag_service_operations = {
+        "mock": [
+            {"name": "mock_node_install", "depends_on": []},
+            {"name": "mock_node_config", "depends_on": ["mock_node_install"]},
+            {"name": "mock_node_start", "depends_on": ["mock_node_config"]},
+            {"name": "mock_node_init", "depends_on": ["mock_node_start"]},
+            {"name": "mock_install", "noop": True, "depends_on": ["mock_node_install"]},
+            {
+                "name": "mock_config",
+                "noop": True,
+                "depends_on": ["mock_install", "mock_node_config"],
+            },
+            {
+                "name": "mock_start",
+                "noop": True,
+                "depends_on": ["mock_config", "mock_node_start"],
+            },
+            {
+                "name": "mock_init",
+                "noop": True,
+                "depends_on": ["mock_start", "mock_node_init"],
+            },
+        ],
+    }
+    service_vars = {
+        "mock": {
+            "mock": {
+                "key": "value",
+                "another_key": "another_value",
+            },
+        },
+    }
+    generate_collection(collection_path, dag_service_operations, service_vars)
+    return Collections.from_collection_list([Collection.from_path(collection_path)])
 
 
 @pytest.fixture(scope="module")
