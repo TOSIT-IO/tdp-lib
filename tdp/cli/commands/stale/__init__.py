ba138d9b86af05b6c44c7ead7b47ec4344caa519
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2023-08-01 17:29:50 +0200

    refactor: move stale generate to core

--- tdp/cli/commands/stale/__init__.py
+++ tdp/cli/commands/stale/__init__.py
@@ -1,7 +1,6 @@
 # Copyright 2022 TOSIT.IO
 # SPDX-License-Identifier: Apache-2.0
 
-from collections import defaultdict
 import click
 from typing import List
 from tabulate import tabulate
@@ -42,54 +41,9 @@ def stale(database_dsn: str, generate: bool, vars, collections):
             deployed_component_version_logs = get_latest_success_component_version_log(
                 session
             )
-            modified_services_or_components_names = (
-                cluster_variables.get_modified_components_names(
-                    services_components_versions=deployed_component_version_logs
-                )
+            stale_components_dict = StaleComponent.generate(
+                dag, cluster_variables, deployed_component_version_logs
             )
-            modified_components_names = set()
-            for modified_component_name in modified_services_or_components_names:
-                click.echo(f"{modified_component_name.full_name} has been modified.")
-                if modified_component_name.is_service:
-                    config_operations_of_the_service = filter(
-                        lambda operation: operation.action_name == "config"
-                        and not operation.is_service_operation(),
-                        dag.services_operations[modified_component_name.service_name],
-                    )
-                    modified_components_names.update(
-                        [
-                            operation.name
-                            for operation in config_operations_of_the_service
-                        ]
-                    )
-                else:
-                    modified_components_names.add(
-                        f"{modified_component_name.full_name}_config"
-                    )
-            operations = dag.get_operations(
-                sources=list(modified_components_names), restart=True
-            )
-            config_and_restart_operations = dag.filter_operations_regex(
-                operations, r".+_(config|(re|)start)"
-            )
-            stale_components_dict = {}
-            for operation in config_and_restart_operations:
-                if operation.is_service_operation():
-                    continue
-                key = (operation.service_name, operation.component_name)
-                stale_component = stale_components_dict.setdefault(
-                    key,
-                    StaleComponent(
-                        service_name=operation.service_name,
-                        component_name=operation.component_name,
-                        to_reconfigure=False,
-                        to_restart=False,
-                    ),
-                )
-                if operation.action_name == "config":
-                    stale_component.to_reconfigure = True
-                if operation.action_name == "restart":
-                    stale_component.to_restart = True
             session.query(StaleComponent).delete()
             session.add_all(list(stale_components_dict.values()))
             session.commit()
