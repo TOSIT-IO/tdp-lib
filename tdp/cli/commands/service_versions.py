2d533ee6683f3a499dcdf1237f6a3e490e2d9ec2
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2023-08-10 17:05:32 +0200

    fix: service-versions command

--- tdp/cli/commands/service_versions.py
+++ tdp/cli/commands/service_versions.py
@@ -4,6 +4,7 @@
 import click
 from tabulate import tabulate
 
+from tdp.cli.commands.browse import _format_component_version_log
 from tdp.cli.queries import get_latest_success_component_version_log
 from tdp.cli.session import get_session_class
 from tdp.cli.utils import database_dsn
@@ -21,19 +22,29 @@ def service_versions(database_dsn):
     session_class = get_session_class(database_dsn)
 
     with session_class() as session:
-        latest_success_service_version = get_latest_success_component_version_log(
+        latest_success_service_version_logs = get_latest_success_component_version_log(
             session
         )
 
-        click.echo(
-            "Service versions:\n"
-            + tabulate(
-                latest_success_service_version,
-                headers=[
-                    ComponentVersionLog.deployment_id.name,
-                    ComponentVersionLog.service.name,
-                    ComponentVersionLog.component.name,
-                    ComponentVersionLog.version.name,
-                ],
-            )
+        if any(latest_success_service_version_logs):
+            # TODO: refactor so that this not import private method from browse.py
+            _print_component_version_logs(latest_success_service_version_logs)
+        else:
+            click.echo("No service has been deployed.")
+
+
+def _print_component_version_logs(component_version_logs: list[ComponentVersionLog]):
+    component_version_log_headers = ComponentVersionLog.__table__.columns.keys()
+
+    click.echo(
+        "Service versions:\n"
+        + tabulate(
+            [
+                _format_component_version_log(
+                    component_version_log, component_version_log_headers
+                )
+                for component_version_log in component_version_logs
+            ],
+            headers="keys",
         )
+    )
