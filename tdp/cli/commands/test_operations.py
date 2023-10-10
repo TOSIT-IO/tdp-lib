af2d45412627dbe258c5bddec4216a396a7cc06b
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2023-10-10 17:32:21 +0200

    feat: add tdp operations command

--- tdp/cli/commands/test_operations.py
+++ tdp/cli/commands/test_operations.py
@@ -5,11 +5,11 @@ from pathlib import Path
 
 from click.testing import CliRunner
 
-from tdp.cli.commands.nodes import nodes
+from tdp.cli.commands.operations import operations
 
 
 def test_tdp_nodes(collection_path: Path):
     args = ["--collection-path", collection_path]
     runner = CliRunner()
-    result = runner.invoke(nodes, args)
+    result = runner.invoke(operations, args)
     assert result.exit_code == 0, result.output
