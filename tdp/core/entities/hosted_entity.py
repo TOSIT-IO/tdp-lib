e59083c25b56c32ec844e208380aea65ed0f0887
Paul Farault <paul@adaltas.com>
2024-01-26 14:33:33 +0100

    refactor: composition on hosted entity

--- tdp/core/entities/hosted_entity.py
+++ tdp/core/entities/hosted_entity.py
@@ -1,7 +1,7 @@
 # Copyright 2022 TOSIT.IO
 # SPDX-License-Identifier: Apache-2.0
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from tdp.core.entities.hostable_entity_name import (
@@ -12,19 +12,67 @@ from tdp.core.entities.hostable_entity_name import (
 )
 
 
-@dataclass(frozen=True)
-class HostedEntity(HostableEntityName, ABC):
-    host: str
+class HostedEntity(ABC):
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def host(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def service(self) -> ServiceName:
+        pass
 
 
 @dataclass(frozen=True)
-class HostedService(HostedEntity, ServiceName):
-    pass
+class HostedService(HostedEntity):
+    _service: ServiceName
+    _host: str
+
+    @property
+    def name(self) -> str:
+        return self._service.name
+
+    @property
+    def host(self) -> str:
+        return self._host
+
+    @property
+    def service(self) -> ServiceName:
+        return self._service
 
 
 @dataclass(frozen=True)
-class HostedServiceComponent(HostedEntity, ServiceComponentName):
-    @classmethod
-    def from_name(cls, name: str, host: str) -> ServiceComponentName:
-        service_name, component_name = name.split("_", 1)
-        return cls(ServiceName(service_name), ComponentName(component_name), host)
+class HostedServiceComponent(HostedEntity):
+    service_component: ServiceComponentName
+    _host: str
+
+    @property
+    def service(self) -> ServiceName:
+        return self.service_component.service
+
+    @property
+    def name(self) -> str:
+        return self.service_component.name
+
+    @property
+    def host(self) -> str:
+        return self._host
+
+    @property
+    def component(self) -> ComponentName:
+        return self.service_component.component
+
+
+def create_hosted_entity(name: HostableEntityName, host: str) -> HostedEntity:
+    if isinstance(name, ServiceName):
+        return HostedService(name, host)
+    elif isinstance(name, ServiceComponentName):
+        return HostedServiceComponent(name, host)
+    else:
+        raise ValueError(f"Unknown name type: {name}")
--- tdp/core/entities/test_hosted_entity.py
+++ tdp/core/entities/test_hosted_entity.py
@@ -1,8 +1,13 @@
-from tdp.core.entities.hosted_entity import HostedService, HostedServiceComponent
+from tdp.core.entities.hostable_entity_name import ServiceComponentName, ServiceName
+from tdp.core.entities.hosted_entity import (
+    HostedService,
+    HostedServiceComponent,
+    create_hosted_entity,
+)
 
 
 def test_hosted_service():
-    hosted_service = HostedService("s", "h")
+    hosted_service = HostedService(ServiceName("s"), "h")
 
     assert isinstance(hosted_service, HostedService)
     assert hosted_service.name == "s"
@@ -10,10 +15,19 @@ def test_hosted_service():
 
 
 def test_hosted_service_component():
-    hosted_service = HostedServiceComponent.from_name("s_c", "h")
+    hosted_service = HostedServiceComponent(ServiceComponentName.from_name("s_c"), "h")
 
     assert isinstance(hosted_service, HostedServiceComponent)
     assert hosted_service.name == "s_c"
     assert str(hosted_service.service) == "s"
     assert str(hosted_service.component) == "c"
     assert hosted_service.host == "h"
+
+
+def test_create_hosted_entity():
+    hosted_service = create_hosted_entity(ServiceName("s"), "h")
+    assert isinstance(hosted_service, HostedService)
+    hosted_service_component = create_hosted_entity(
+        ServiceComponentName.from_name("s_c"), "h"
+    )
+    assert isinstance(hosted_service_component, HostedServiceComponent)
