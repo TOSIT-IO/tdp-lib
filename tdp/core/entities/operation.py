4783ada7f28ddaecf32100c720fb0e6f965d134c
Paul Farault <paul@adaltas.com>
2024-02-13 10:44:10 +0100

    refactor: dag operation

--- tdp/core/entities/operation.py
+++ tdp/core/entities/operation.py
@@ -5,20 +5,36 @@ from __future__ import annotations
 
 from abc import ABC
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
 
 from tdp.core.constants import HOST_NAME_MAX_LENGTH
 from tdp.core.entities.operation_name import OperationName
+
+# TODO: create constructors
 
 
 @dataclass(frozen=True)
 class Operation(ABC):
     name: OperationName
-    collection_name: str
-    depends_on: set[str]
 
 
 @dataclass(frozen=True)
-class PlaybookOperation(Operation):
+class DagOperation(Operation):
+    depends_on: set[str]
+    playbook: Optional[PlaybookOperation]
+
+
+@dataclass(frozen=True)
+class OtherOperation(Operation):
+    playbook: PlaybookOperation
+    pass
+
+
+@dataclass(frozen=True)
+class PlaybookOperation:
+    playbook_path: Path
+    collection_name: str
     host_names: set[str]
 
     def __post_init__(self):
@@ -28,8 +44,3 @@ class PlaybookOperation(Operation):
                     f"Host '{host_name}' must be less than {HOST_NAME_MAX_LENGTH} "
                     "characters."
                 )
-
-
-@dataclass(frozen=True)
-class NoOperation(Operation):
-    pass
