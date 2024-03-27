6b7aaf277678bdbe86c9a8abc0b5515f0a34ccf7
Paul Farault <paul@adaltas.com>
2024-03-27 10:35:42 +0100

    refactor: rename get_sch_status to get_cluster_status

--- tdp/cli/commands/status/show.py
+++ tdp/cli/commands/status/show.py
@@ -95,7 +95,7 @@ def show(
 
     with Dao(database_dsn) as dao:
         print_sch_status_logs(
-            dao.get_sch_status(
+            dao.get_cluster_status(
                 service,
                 component,
                 hosts,
