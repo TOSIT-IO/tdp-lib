d7823a23c4d3af27ba45a5cc134713bb7f91b36b
Paul Farault <paul@adaltas.com>
2024-11-07 11:39:07 +0100

    refactor: merge CollectionReader methods

--- tdp/core/collection.py
+++ tdp/core/collection.py
@@ -51,6 +51,23 @@ class MissingMandatoryDirectoryError(Exception):
     pass
 
 
+class TDPLibDagNodeModel(BaseModel):
+    """Model for a TDP operation defined in a tdp_lib_dag file."""
+
+    model_config = ConfigDict(extra="ignore")
+
+    name: str
+    depends_on: list[str] = []
+
+
+class TDPLibDagModel(BaseModel):
+    """Model for a TDP DAG defined in a tdp_lib_dag file."""
+
+    model_config = ConfigDict(extra="ignore")
+
+    operations: list[TDPLibDagNodeModel]
+
+
 class CollectionReader:
     """An enriched version of an Ansible collection.
 
@@ -73,7 +90,7 @@ class CollectionReader:
             MissingMandatoryDirectoryError: If the collection does not contain a mandatory directory.
         """
         self._path = Path(path)
-        check_collection_structure(self._path)
+        self._check_collection_structure(self._path)
         self._inventory_reader = inventory_reader or InventoryReader()
 
     # ? Is this method really useful?
@@ -124,93 +141,63 @@ class CollectionReader:
 
     def read_dag_nodes(self) -> Generator[TDPLibDagNodeModel, None, None]:
         """Read the DAG nodes stored in the dag_directory."""
-        return read_dag_directory(self.dag_directory)
+        for dag_file in (self.dag_directory).glob("*" + YML_EXTENSION):
+            with dag_file.open("r") as operations_file:
+                file_content = yaml.load(operations_file, Loader=Loader)
+
+            try:
+                tdp_lib_dag = TDPLibDagModel(operations=file_content)
+                for operation in tdp_lib_dag.operations:
+                    yield operation
+            except ValidationError as e:
+                logger.error(f"Error while parsing tdp_lib_dag file {dag_file}: {e}")
+                raise
 
     def read_playbooks(self) -> dict[str, Playbook]:
         """Read the playbooks stored in the playbooks_directory."""
-        return read_playbooks_directory(
-            self.playbooks_directory,
-            self.name,
-            inventory_reader=self._inventory_reader,
-        )
+        return {
+            playbook_path.stem: Playbook(
+                path=playbook_path,
+                collection_name=self.name,
+                hosts=read_hosts_from_playbook(playbook_path, self._inventory_reader),
+            )
+            for playbook_path in (self.playbooks_directory).glob("*" + YML_EXTENSION)
+        }
 
     def read_schemas(self) -> list[ServiceCollectionSchema]:
-        """Read the schemas stored in the schema_directory."""
-        return read_schema_directory(self.schema_directory)
+        """Read the schemas stored in the schema_directory.
 
+        Invalid schemas are ignored.
+        """
+        schemas: list[ServiceCollectionSchema] = []
+        for schema_path in (self.schema_directory).glob("*" + JSON_EXTENSION):
+            try:
+                schemas.append(ServiceCollectionSchema.from_path(schema_path))
+            except InvalidSchemaError as e:
+                logger.warning(f"{e}. Ignoring schema.")
+        return schemas
 
-def check_collection_structure(path: Path) -> None:
-    """Check the structure of a collection.
+    def _check_collection_structure(self, path: Path) -> None:
+        """Check the structure of a collection.
 
-    Args:
-        path: Path to the collection.
+        Args:
+            path: Path to the collection.
 
-    Raises:
-        PathDoesNotExistsError: If the path does not exists.
-        PathIsNotADirectoryError: If the path is not a directory.
-        MissingMandatoryDirectoryError: If the collection does not contain a mandatory directory.
-    """
-    if not path.exists():
-        raise PathDoesNotExistsError(f"{path} does not exists.")
-    if not path.is_dir():
-        raise PathIsNotADirectoryError(f"{path} is not a directory.")
-    for mandatory_directory in MANDATORY_DIRECTORIES:
-        mandatory_path = path / mandatory_directory
-        if not mandatory_path.exists() or not mandatory_path.is_dir():
-            raise MissingMandatoryDirectoryError(
-                f"{path} does not contain the mandatory directory {mandatory_directory}.",
-            )
-
-
-def read_schema_directory(directory_path: Path) -> list[ServiceCollectionSchema]:
-    """Read the schemas from a directory.
-
-    This function is meant to be used only once during the initialization of a
-    collection object.
-
-    Invalid schemas are ignored.
-
-    Args:
-        directory_path: Path to the schema directory.
-
-    Returns:
-        Dictionary of schemas.
-    """
-    schemas: list[ServiceCollectionSchema] = []
-    for schema_path in (directory_path).glob("*" + JSON_EXTENSION):
-        try:
-            schemas.append(ServiceCollectionSchema.from_path(schema_path))
-        except InvalidSchemaError as e:
-            logger.warning(f"{e}. Ignoring schema.")
-    return schemas
-
-
-def read_playbooks_directory(
-    directory_path: Path,
-    collection_name: str,
-    inventory_reader: Optional[InventoryReader] = None,
-) -> dict[str, Playbook]:
-    """Read the playbooks from a directory.
-
-    This function is meant to be used only once during the initialization of a
-    collection object.
-
-    Args:
-        directory_path: Path to the playbooks directory.
-        collection_name: Name of the collection.
-        inventory_reader: Inventory reader.
-
-    Returns:
-        Dictionary of playbooks.
-    """
-    return {
-        playbook_path.stem: Playbook(
-            playbook_path,
-            collection_name,
-            read_hosts_from_playbook(playbook_path, inventory_reader),
-        )
-        for playbook_path in (directory_path).glob("*" + YML_EXTENSION)
-    }
+        Raises:
+            PathDoesNotExistsError: If the path does not exists.
+            PathIsNotADirectoryError: If the path is not a directory.
+            MissingMandatoryDirectoryError: If the collection does not contain a mandatory directory.
+        """
+        if not path.exists():
+            raise PathDoesNotExistsError(f"{path} does not exists.")
+        if not path.is_dir():
+            raise PathIsNotADirectoryError(f"{path} is not a directory.")
+        for mandatory_directory in MANDATORY_DIRECTORIES:
+            mandatory_path = path / mandatory_directory
+            if not mandatory_path.exists() or not mandatory_path.is_dir():
+                raise MissingMandatoryDirectoryError(
+                    f"{path} does not contain the mandatory directory {mandatory_directory}.",
+                )
 
 
 def read_hosts_from_playbook(
@@ -232,51 +219,3 @@ def read_hosts_from_playbook(
             return inventory_reader.get_hosts_from_playbook(fd)
     except Exception as e:
         raise ValueError(f"Can't parse playbook {playbook_path}.") from e
-
-
-def read_dag_directory(
-    directory_path: Path,
-) -> Generator[TDPLibDagNodeModel, None, None]:
-    """Read the DAG files from a directory.
-
-    Args:
-        directory_path: Path to the DAG directory.
-
-    Returns:
-        List of DAG nodes.
-    """
-    for dag_file in (directory_path).glob("*" + YML_EXTENSION):
-        yield from read_dag_file(dag_file)
-
-
-class TDPLibDagNodeModel(BaseModel):
-    """Model for a TDP operation defined in a tdp_lib_dag file."""
-
-    model_config = ConfigDict(extra="ignore")
-
-    name: str
-    depends_on: list[str] = []
-
-
-class TDPLibDagModel(BaseModel):
-    """Model for a TDP DAG defined in a tdp_lib_dag file."""
-
-    model_config = ConfigDict(extra="ignore")
-
-    operations: list[TDPLibDagNodeModel]
-
-
-def read_dag_file(
-    dag_file_path: Path,
-) -> Generator[TDPLibDagNodeModel, None, None]:
-    """Read a tdp_lib_dag file and return a list of DAG operations."""
-    with dag_file_path.open("r") as operations_file:
-        file_content = yaml.load(operations_file, Loader=Loader)
-
-    try:
-        tdp_lib_dag = TDPLibDagModel(operations=file_content)
-        for operation in tdp_lib_dag.operations:
-            yield operation
-    except ValidationError as e:
-        logger.error(f"Error while parsing tdp_lib_dag file {dag_file_path}: {e}")
-        raise
--- tests/unit/core/test_collection.py
+++ tests/unit/core/test_collection.py
@@ -11,11 +11,7 @@ from tdp.core.collection import (
     MissingMandatoryDirectoryError,
     PathDoesNotExistsError,
     PathIsNotADirectoryError,
-    check_collection_structure,
-    read_dag_directory,
-    read_dag_file,
     read_hosts_from_playbook,
-    read_playbooks_directory,
 )
 from tdp.core.constants import (
     DAG_DIRECTORY_NAME,
@@ -26,6 +22,18 @@ from tests.conftest import generate_collection_at_path
 from tests.unit.core.models.test_deployment_log import (
     MockInventoryReader,
 )
+
+
+@pytest.fixture(scope="session")
+def mock_empty_collection_reader(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    temp_collection_path = tmp_path_factory.mktemp("mock_collection")
+    for directory in [
+        DAG_DIRECTORY_NAME,
+        DEFAULT_VARS_DIRECTORY_NAME,
+        PLAYBOOKS_DIRECTORY_NAME,
+    ]:
+        (temp_collection_path / directory).mkdir(parents=True, exist_ok=True)
+    return temp_collection_path
 
 
 def test_collection_from_path_does_not_exist():
@@ -82,14 +90,10 @@ def test_read_hosts_from_playbook(tmp_path: Path):
     assert hosts == {"host1", "host2"}
 
 
-def test_init_collection_playbooks(tmp_path: Path):
-    collection_path = tmp_path / "collection"
-    playbook_directory = "playbooks"
-    (playbook_directory_path := collection_path / playbook_directory).mkdir(
-        parents=True, exist_ok=True
-    )
-    playbook_path_1 = playbook_directory_path / "playbook1.yml"
-    playbook_path_2 = playbook_directory_path / "playbook2.yml"
+def test_collection_reader_read_playbooks(mock_empty_collection_reader: Path):
+    collection_reader = CollectionReader(mock_empty_collection_reader)
+    playbook_path_1 = collection_reader.playbooks_directory / "playbook1.yml"
+    playbook_path_2 = collection_reader.playbooks_directory / "playbook2.yml"
     playbook_path_1.write_text(
         """---
 - name: Play 1
@@ -108,101 +112,20 @@ def test_init_collection_playbooks(tmp_path: Path):
       command: echo "Hello, GitHub Copilot!"
 """
     )
-    playbooks = read_playbooks_directory(playbook_directory_path, collection_path.name)
+    playbooks = collection_reader.read_playbooks()
     assert len(playbooks) == 2
     assert "playbook1" in playbooks
     assert "playbook2" in playbooks
     assert playbooks["playbook1"].path == playbook_path_1
-    assert playbooks["playbook1"].collection_name == collection_path.name
+    assert playbooks["playbook1"].collection_name == collection_reader.name
     assert playbooks["playbook2"].path == playbook_path_2
-    assert playbooks["playbook2"].collection_name == collection_path.name
+    assert playbooks["playbook2"].collection_name == collection_reader.name
 
 
-def test_check_collection_structure_path_does_not_exist(tmp_path: Path):
-    with pytest.raises(PathDoesNotExistsError):
-        check_collection_structure(tmp_path / "nonexistent_directory")
-
-
-def test_check_collection_structure_path_is_not_a_directory(tmp_path: Path):
-    empty_file = tmp_path / "foo"
-    empty_file.touch()
-    with pytest.raises(PathIsNotADirectoryError):
-        check_collection_structure(empty_file)
-
-
-def test_check_collection_structure_missing_mandatory_directory(tmp_path: Path):
-    with pytest.raises(MissingMandatoryDirectoryError):
-        check_collection_structure(tmp_path)
-
-
-def test_check_collection_structure_valid_collection(tmp_path: Path):
-    collection_path = tmp_path / "collection"
-    for mandatory_directory in (
-        DAG_DIRECTORY_NAME,
-        DEFAULT_VARS_DIRECTORY_NAME,
-        PLAYBOOKS_DIRECTORY_NAME,
-    ):
-        (collection_path / mandatory_directory).mkdir(parents=True, exist_ok=True)
-    assert check_collection_structure(collection_path) is None
-
-
-def test_read_dag_file(tmp_path: Path):
-    dag_file_path = tmp_path / "dag_file.yml"
-    dag_file_path.write_text(
-        """---
-- name: s1_c1_a
-  depends_on:
-    - sx_cx_a
-- name: s2_c2_a
-  depends_on:
-    - s1_c1_a
-- name: s3_c3_a
-  depends_on:
-    - sx_cx_a
-    - sy_cy_a
-"""
-    )
-    operations = list(read_dag_file(dag_file_path))
-    assert len(operations) == 3
-    assert operations[0].name == "s1_c1_a"
-    assert operations[0].depends_on == ["sx_cx_a"]
-    assert operations[1].name == "s2_c2_a"
-    assert operations[1].depends_on == ["s1_c1_a"]
-    assert operations[2].name == "s3_c3_a"
-    assert operations[2].depends_on == ["sx_cx_a", "sy_cy_a"]
-
-
-def test_read_dag_file_empty(tmp_path: Path):
-    dag_file_path = tmp_path / "dag_file.yml"
-    dag_file_path.write_text("")
-    with pytest.raises(ValidationError):
-        list(read_dag_file(dag_file_path))
-
-
-def test_read_dag_file_with_additional_props(tmp_path: Path):
-    dag_file_path = tmp_path / "dag_file.yml"
-    dag_file_path.write_text(
-        """---
-- name: s1_c1_a
-  depends_on:
-    - sx_cx_a
-  foo: bar
-"""
-    )
-    operations = list(read_dag_file(dag_file_path))
-    assert len(operations) == 1
-    assert operations[0].name == "s1_c1_a"
-    assert operations[0].depends_on == ["sx_cx_a"]
-
-
-def test_get_collection_dag_nodes(tmp_path: Path):
-    collection_path = tmp_path / "collection"
-    dag_directory = "dag"
-    (dag_directory_path := collection_path / dag_directory).mkdir(
-        parents=True, exist_ok=True
-    )
-    dag_file_1 = dag_directory_path / "dag1.yml"
-    dag_file_2 = dag_directory_path / "dag2.yml"
+def test_collection_reader_read_dag_nodes(mock_empty_collection_reader: Path):
+    collection_reader = CollectionReader(mock_empty_collection_reader)
+    dag_file_1 = collection_reader.dag_directory / "dag1.yml"
+    dag_file_2 = collection_reader.dag_directory / "dag2.yml"
     dag_file_1.write_text(
         """---
 - name: s1_c1_a
@@ -217,7 +140,7 @@ def test_get_collection_dag_nodes(tmp_path: Path):
     - s1_c1_a
 """
     )
-    dag_nodes = list(read_dag_directory(dag_directory_path))
+    dag_nodes = list(collection_reader.read_dag_nodes())
     assert len(dag_nodes) == 2
     assert any(
         node.name == "s1_c1_a" and node.depends_on == ["sx_cx_a"] for node in dag_nodes
@@ -225,3 +148,13 @@ def test_get_collection_dag_nodes(tmp_path: Path):
     assert any(
         node.name == "s2_c2_a" and node.depends_on == ["s1_c1_a"] for node in dag_nodes
     )
+
+
+def test_collection_reader_read_dag_nodes_empty_file(
+    mock_empty_collection_reader: Path,
+):
+    collection_reader = CollectionReader(mock_empty_collection_reader)
+    dag_file = collection_reader.dag_directory / "dag.yml"
+    dag_file.write_text("")
+    with pytest.raises(ValidationError):
+        list(collection_reader.read_dag_nodes())
