2f88f926487108b60f23b88fe0544010943b2019
Paul Farault <paul@adaltas.com>
2024-11-07 11:13:42 +0100

    refactor: rename collection module to collection_reader

--- tests/unit/core/test_collection_reader.py
+++ tests/unit/core/test_collection_reader.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import pytest
 from pydantic import ValidationError
 
-from tdp.core.collection import (
+from tdp.core.collection_reader import (
     CollectionReader,
     MissingMandatoryDirectoryError,
     PathDoesNotExistsError,
