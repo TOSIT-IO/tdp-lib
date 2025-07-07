a48f660917a03ca20604e2f075a03c8f61afca01
Paul Farault <paul@adaltas.com>
2025-07-07 17:26:37 +0200

    feat: extract db engine creation logic in core

--- tdp/cli/params.py
+++ tdp/cli/params.py
@@ -131,7 +131,7 @@ def database_dsn_option(func: FC) -> FC:
     Return a SQLAlchemy Engine instance, available as "db_engine" in the command context.
     """
 
-    from sqlalchemy import create_engine
+    from tdp.core.db import get_engine
 
     return click.option(
         "db_engine",
@@ -139,7 +139,7 @@ def database_dsn_option(func: FC) -> FC:
         envvar="TDP_DATABASE_DSN",
         required=True,
         type=str,
-        callback=lambda _ctx, _param, value: create_engine(value, future=True),
+        callback=lambda _ctx, _param, value: get_engine(value),
         help=(
             "Database Data Source Name, in sqlalchemy driver form "
             "example: sqlite:////data/tdp.db or sqlite+pysqlite:////data/tdp.db. "
