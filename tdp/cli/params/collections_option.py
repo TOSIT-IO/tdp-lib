1b8e5b82a9e54730688ede1c6391d45aa46716e8
Paul Farault <paul@adaltas.com>
2025-07-07 16:49:09 +0200

    refactor: define callbacks inside options

--- tdp/cli/params/collections_option.py
+++ tdp/cli/params/collections_option.py
@@ -6,36 +6,15 @@ from pathlib import Path
 import click
 from click.decorators import FC
 
-from tdp.core.collections import Collections
-
-
-def _collections_from_paths(
-    ctx: click.Context, param: click.Parameter, value: list[Path]
-) -> Collections:
-    """Transforms a list of paths into a Collections object.
-
-    Args:
-        ctx: Click context.
-        param: Click parameter.
-        value: List of collections paths.
-
-    Returns:
-        Collections object from the paths.
-
-    Raises:
-        click.BadParameter: If the value is empty.
-    """
-    if not value:
-        raise click.BadParameter("cannot be empty", ctx=ctx, param=param)
-
-    return Collections.from_collection_paths(value)
-
 
 def collections_option(func: FC) -> FC:
     """Click option for the collection path.
 
     Takes multiple paths (required) and transforms them into a Collections object.
     """
+
+    from tdp.core.collections import Collections
+
     return click.option(
         "--collection-path",
         "collections",
@@ -43,7 +22,7 @@ def collections_option(func: FC) -> FC:
         required=True,
         multiple=True,
         type=click.Path(resolve_path=True, path_type=Path),
-        callback=_collections_from_paths,
+        callback=lambda _ctx, _param, value: Collections.from_collection_paths(value),
         help="Path to the collection. Can be used multiple times.",
         is_eager=True,  # This option is used by other options, so we need to parse it first
     )(func)
