b0b1796e12a43b1936c3df5a80faf3131839e42f
Paul Farault <paul@adaltas.com>
2025-07-07 16:49:09 +0200

    refactor: define callbacks inside options

--- tdp/cli/params/database_dsn_option.py
+++ tdp/cli/params/database_dsn_option.py
@@ -3,25 +3,12 @@
 
 import click
 from click.decorators import FC
-from sqlalchemy import Engine, create_engine
-
-
-def _engine_from_dsn(ctx: click.Context, param: click.Parameter, value: str) -> Engine:
-    """Transforms a database DSN into an engine.
-
-    Args:
-        ctx: Click context.
-        param: Click parameter.
-        value: Database DSN.
-
-    Returns:
-        Engine from the DSN.
-    """
-    return create_engine(value, future=True)
 
 
 def database_dsn_option(func: FC) -> FC:
     """Click option that adds a database_dsn option to the command."""
+
+    from sqlalchemy import create_engine
 
     return click.option(
         "db_engine",
@@ -29,7 +16,7 @@ def database_dsn_option(func: FC) -> FC:
         envvar="TDP_DATABASE_DSN",
         required=True,
         type=str,
-        callback=_engine_from_dsn,
+        callback=lambda _ctx, _param, value: create_engine(value, future=True),
         help=(
             "Database Data Source Name, in sqlalchemy driver form "
             "example: sqlite:////data/tdp.db or sqlite+pysqlite:////data/tdp.db. "
